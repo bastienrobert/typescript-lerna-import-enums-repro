e1f911e2c69e4a07fdbcdca9ac4388baa39d865c
Bastien Robert <bastienrobert@outlook.com>
2020-11-12 19:56:51 +0100

    chore: init

---
+++ packages/bar/index.ts
@@ -0,0 +1,3 @@
+export enum Baz {
+  String = 'STRING',
+}
---
+++ packages/foo/index.ts
@@ -0,0 +1,5 @@
+import { Baz } from '@test/bar'
+
+console.log('====================')
+console.log(Baz.String)
+console.log('====================')
